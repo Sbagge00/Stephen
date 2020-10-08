6f96f80ec6494ea79112db0e9578dfad60e4e791
Stephen <31675411+Sbagge00@users.noreply.github.com>
2020-10-08 14:59:58 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,26 @@
+var character = document.getElementById("dino");
+var deadcharacter = document.getElementById("skeleton");
+var bullet = document.getElementById("block");
+function jump(){
+    if(dino.classList != "animate"){
+        dino.classList.add("animate");
+    }
+    setTimeout(function(){
+        dino.classList.remove("animate");
+    },500);
+}
+var checkDead = setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    var bulletLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(bulletLeft<20 && bulletLeft>0 && characterTop>=130){
+        block.style.display = "none";
+        block.style.animation = "none";
+        dino.style.display = "none";
+        dino.style.animation = "none";
+        skeleton.style.display = 'block';
+        skeleton.style.animation = 'block';
+        alert("GAME OVER");
+    }
+},10);
